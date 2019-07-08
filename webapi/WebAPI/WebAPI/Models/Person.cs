a90cf591cde89fdcbed81e7141471c690f093195
Jose Moreira <jose.ourique.moreira@gmail.com>
2019-07-08 11:40:35 +0000

    Person Object Created :)

--- webapi/WebAPI/WebAPI/Models/Person.cs
+++ webapi/WebAPI/WebAPI/Models/Person.cs
@@ -5,7 +5,48 @@ using System.Web;
 
 namespace WebAPI.Models
 {
-    public class Class1
+    public class Person
     {
+        private String firstName;
+        private String lastName;
+        private int age;
+
+        public string setFirstName(string firstName)
+        {
+            return this.firstName = firstName;
+        }
+
+        public string setLastName(string lastName)
+        {
+            return this.lastName = lastName;
+        }
+
+        public int setAge(int age)
+        {
+            return this.age = age;
+        }
+
+
+        private string getfirstName()
+        {
+            return firstName;
+        }
+
+        private string getLastName()
+        {
+            return lastName;
+        }
+
+        private int getAge()
+        {
+            return age;
+        }
+
+
+        public string PersonInfo() {
+
+            return "O meu nome é " + getfirstName() + " " + getLastName() + "e tenho " + getAge() + " Anos.";
+
+        }
     }
 }
