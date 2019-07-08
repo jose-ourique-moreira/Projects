d82a938718d9c8f7e0f28f559b2217231f176abb
Jose Moreira <jose.ourique.moreira@gmail.com>
2019-07-08 20:10:05 +0000

    API HELLO WORLD TEST

--- webapi/WebAPI/WebAPI/Controllers/MainController.cs
+++ webapi/WebAPI/WebAPI/Controllers/MainController.cs
@@ -10,44 +10,16 @@ namespace WebAPI.Controllers
 {
     public class MainController : ApiController
     {
-
+       
         int[] unsortedList = { 0, 10, 16, 21, 33, 50, 45, 23, 80 };
 
-         public HttpWebResponse OrderList()
+        
+        public string HelloWorld()
         {
-            Array.Sort(unsortedList);
 
-            HttpWebResponse response = new HttpWebResponse();
-
-            response.Content = new StringContent(unsortedList.ToString);
-
-        }
-
-        public HttpWebResponse OddsOrEvens(int number)
-        {
-            HttpWebResponse response = new HttpWebResponse();
-
-            if(number%2 == 0)
-            {
-                response.Content = new StringContent("The number is Even!");
-            }
-            else
-            {
-                response.Content = new StringContent("The number is Odd!");
-            }
-
-
+            return "Hello World";
         }
 
 
-
-    
-       
-
-
-
-  
-
-
-    }
+        }
 }
